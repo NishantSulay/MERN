50ac4d266bb43e48924003145509862f8a0a95f9
Nishant Sulay <nishantsulay@hotmail.com>
2023-01-30 16:42:58 -0500

    Update post.js
    (1) update post routes logic for read and update

--- server/controllers/post.js
+++ server/controllers/post.js
@@ -29,3 +29,46 @@ export const createPost = async (req, res) => {
     }
 
 };
+
+
+/* READ */
+export const getPosts = async (req, res) => {
+    try{
+        const posts = await Post.find();
+        res.status(200).json(posts);
+    } catch (err){
+        res.status(404).json({message: err.message});
+    }
+};
+
+export const getUserPost = async (req, res) => {
+    try{
+        const posts = await Post.find({userId: req.params.id});
+        res.status(200).json(posts);
+
+    }catch (err){
+        res.status(404).json({message: err.message});
+    }
+}
+
+/* UPDATE */
+export const likePost = async (req, res) => {
+    try{
+        const {id} = req.params;
+        const {userId} = req.body;
+        const post = await Post.findById(id);
+        const isLiked = post.likes.get(userId);
+
+        if(isLiked){
+            post.likes.delete(userId);
+        } else {
+            post.likes.set(userId, true);
+        }
+
+        const updatedPost = await Post.findOneAndUpdate(id, {likes: post.likes}, {new: true});
+
+        res.status(200).json(updatedPost);
+    } catch(err) {
+        res.status(404).json({message: err.message});
+    }
+}
